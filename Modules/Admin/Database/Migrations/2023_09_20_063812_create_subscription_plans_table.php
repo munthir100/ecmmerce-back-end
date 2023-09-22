747fe0f4267f241a56e0f8486914b4c24e5c3a5a
munthir100 <munthiromer100@gmail.com>
2023-09-22 09:35:58 +0200

    add the subscriptions plans and integrate with stripe and use laravel casheir package , and set the default subsctiption for new store is basic for year

---
+++ Modules/Admin/Database/Migrations/2023_09_20_063812_create_subscription_plans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscription_plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('interval');
+            $table->integer('interval_count')->default(1);
+            $table->integer('amount');
+            $table->string('stripe_plan_id')->unique();
+            $table->string('stripe_plan_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscription_plans');
+    }
+};
